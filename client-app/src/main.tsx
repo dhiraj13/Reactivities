adfaa4fefe91d943b418410a1c367c4502342cc2
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-05 21:35:51 +0545

    folder structure in react

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'semantic-ui-css/semantic.min.css'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/layout/App";
 
-createRoot(document.getElementById('root')!).render(
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
