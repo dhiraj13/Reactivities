afed7a8c566b791e12907b6d388ac0caf8aa5827
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-06 12:53:31 +0545

    delete an activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -55,6 +55,10 @@ function App() {
     setSelectedActivity(activity);
   }
 
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(x => x.id != id)])
+  }
+
   return (
     <>
       <NavBar openForm={handleFormOpen} />
@@ -68,6 +72,7 @@ function App() {
           editMode={editMode}
           openForm={handleFormOpen}
           createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
         />
       </Container>
     </>
