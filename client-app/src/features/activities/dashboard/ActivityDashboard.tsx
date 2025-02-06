662f69a619a82d5c8700d5ee06c37c1e3d50e41b
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-06 11:35:19 +0545

    create a details view

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,8 @@
 import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import { When } from "react-if";
 
 interface Props {
   activities: Activity[];
@@ -11,6 +13,11 @@ export default function ActivityDashboard({ activities }: Props) {
     <Grid>
       <Grid.Column width="10">
         <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width="6">
+        <When condition={activities.length > 0}>
+          <ActivityDetails activity={activities[0]} />
+        </When>
       </Grid.Column>
     </Grid>
   );
