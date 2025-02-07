4e022870bf855af4022c806dada115fe5902f5a1
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-07 10:49:47 +0545

    install react router

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app/layout/App";
-
 import "semantic-ui-css/semantic.min.css";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
 import "./app/layout/styles.css";
+import { router } from "./app/router/Routes";
 import { store, StoreContext } from "./app/stores/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <StoreContext.Provider value={store}>
-      <App />
+      <RouterProvider router={router} />
     </StoreContext.Provider>
   </StrictMode>
 );
