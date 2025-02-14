7dcbbbefea6c1eedf1227a0c1024ea4807ff11b6
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-14 12:13:48 +0545

    add a custom auth policty

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -26,7 +26,7 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
-        private readonly IUserAccessor _userAccessor;
+            private readonly IUserAccessor _userAccessor;
             public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _userAccessor = userAccessor;
