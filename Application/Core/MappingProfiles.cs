98706e444f6102edb76b0d2c3c89dd787b65f343
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-20 18:19:50 +0545

    return an attendee dto

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -12,10 +12,11 @@ namespace Application.Core
             CreateMap<Activity, ActivityDto>()
                 .ForMember(dest => dest.HostUsername, opt => opt.MapFrom(src => src.Attendees
                 .FirstOrDefault(x => x.IsHost).AppUser.UserName));
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.AppUser.UserName))
-                .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.AppUser.Bio));
+                .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.AppUser.Bio))
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
