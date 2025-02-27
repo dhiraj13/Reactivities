324e4560f8e709f2342fe53910d730f71a3dbfc8
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-27 11:47:24 +0545

    add the following property to the mapping configuration

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -9,6 +9,7 @@ namespace Application.Core
     {
         public MappingProfiles()
         {
+            string currentUsername = null;
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
                 .ForMember(dest => dest.HostUsername, opt => opt.MapFrom(src => src.Attendees
@@ -21,7 +22,9 @@ namespace Application.Core
             CreateMap<AppUser, Profiles.Profile>()
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.FollowersCount, opt => opt.MapFrom(src => src.Followers.Count))
-                .ForMember(dest => dest.FollowingCount, opt => opt.MapFrom(src => src.Followings.Count));
+                .ForMember(dest => dest.FollowingCount, opt => opt.MapFrom(src => src.Followings.Count))
+                .ForMember(dest => dest.Following,
+                    opt => opt.MapFrom(src => src.Followers.Any(x => x.Observer.UserName == currentUsername)));
             CreateMap<Comment, CommentDto>()
                 .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.Author.DisplayName))
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Author.UserName))
