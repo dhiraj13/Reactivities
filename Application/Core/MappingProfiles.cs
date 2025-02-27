76eb4d8fac4144e1e5ab4f871921a65b87676a2f
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-27 18:21:05 +0545

    update the other handlers with the following property

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -18,7 +18,12 @@ namespace Application.Core
                 .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.AppUser.UserName))
                 .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.AppUser.Bio))
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+                .ForMember(dest => dest.Image,
+                    opt => opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.FollowersCount, opt => opt.MapFrom(src => src.AppUser.Followers.Count))
+                .ForMember(dest => dest.FollowingCount, opt => opt.MapFrom(src => src.AppUser.Followings.Count))
+                .ForMember(dest => dest.Following,
+                    opt => opt.MapFrom(src => src.AppUser.Followers.Any(x => x.Observer.UserName == currentUsername)));
             CreateMap<AppUser, Profiles.Profile>()
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.FollowersCount, opt => opt.MapFrom(src => src.Followers.Count))
